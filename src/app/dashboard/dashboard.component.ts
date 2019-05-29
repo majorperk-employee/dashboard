f7c04fcfa6be47c65d9ede85fd48164ed0c0a756
Sergei Hanka <sergeiahanka@gmail.com>
2019-05-28 22:33:09 -0500

    updated dash colors

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,37 +12,43 @@ export class DashboardComponent {
       title: 'Average Retention',
       abbreviation: 'Av. Retention',
       value: 50,
-      suffix: "%"
+      suffix: "%",
+      color: '#59acfa'
     },
     {
       title: 'Productivity',
       abbreviation: "Productivity",
       value: 50,
-      suffix: "%"
+      suffix: "%",
+      color: '#febf0e'
     },
     {
       title: 'Average Employee Performance Score',
       abbreviation: 'Av. Empl Perf.',
       value: 50,
-      suffix: "pts"
+      suffix: "pts",
+      color: '#f6851e'
     },
     {
       title: 'Number of Employees Left ?',
       abbreviation: '# of Empl Left ?',
       value: 50,
-      suffix: ""
+      suffix: "",
+      color: '#ef4822'
     },
     {
       title: 'Active Employee Count',
       abbreviation: 'Active Empl #',
       value: 50,
-      suffix: ""
+      suffix: "",
+      color: '#8dd26c'
     },
     {
       title: 'New Employee Count',
       abbreviation: 'New Empl #',
       value: 50,
-      suffix: ""
+      suffix: "",
+      color: '#806cd2'
     }
   ];
 
